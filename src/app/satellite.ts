ab003bab4b9904833ee89803780e7834ef0ef8ca
Sharon Konki <sharonkonki@gmail.com>
2021-12-16 20:11:55 -0600

    highlighted space debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,7 +15,13 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-		return true;
+		if(this.type.toLowerCase() === "space debris"){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
    }
 
 }
